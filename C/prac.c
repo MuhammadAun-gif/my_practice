b58cadd9da36eb7ed93d183d8d1bf6aa9103eff1
Muhammad Aun <muhammadaunnaqavi@gmail.com>
2024-06-21 14:43:54 +0500

    Added create_node and traversing function to my program

--- C/prac.c
+++ C/prac.c
@@ -1,8 +1,40 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* next; // next is a pointer of type struct node.
+};
+
+typedef struct node* nodeptr;
+
+void linkedlistTraversal(nodeptr);
+nodeptr create_node(int);
 
 int main (void)
 {
-    printf("Hello World!\n");
-    printf("Hello World!\n");
+    // Creating Head node.
+    nodeptr head = create_node(4);
+
+    // Traversing the list.
+    linkedlistTraversal(head);
+
     return 0;
+
+}
+
+void linkedlistTraversal(nodeptr ptr)
+{
+    while(ptr!= NULL)
+    {
+        printf("Element:%d\n",ptr -> data);
+        ptr = ptr -> next;
+    }
+}
+
+nodeptr create_node(int item)
+{
+    nodeptr newnode = (nodeptr)malloc(sizeof(struct node));
+    newnode -> data = item;
+    newnode -> next = NULL;
 }
